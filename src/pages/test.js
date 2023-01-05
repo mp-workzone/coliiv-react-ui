dae411b9da4d80233d3cc2604d42ec02787ac122
Sakib Anjum <sakib.abh@gmail.com>
2023-01-05 20:26:35 +0600

    react router setup

---
+++ src/pages/test.js
@@ -0,0 +1,19 @@
+// @flow
+import Gallery from "../components/gallery";
+import Header from "../components/header";
+import Hero from "../components/hero";
+import Service from "../components/service";
+import VideoArea from "../components/slogan";
+function Home() {
+  return (
+    <>
+      <Header />,
+      <Hero />,
+      <VideoArea />,
+      <Service />,
+      <Gallery />
+    </>
+  );
+}
+
+export default Home;
