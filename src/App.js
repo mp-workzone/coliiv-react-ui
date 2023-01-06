aa97d3c06038240fedc99f50cc8afa7d5b07e29a
Sakib Anjum <sakib.abh@gmail.com>
2023-01-07 00:05:06 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import "./css/common.css";
 import Home from "./pages/home";
-import Test from "./pages/test";
 function App() {
   return (
     <div className="main">
       <Header />
-      <Routes>
+      <Home/>
+      {/* <Routes>
         <Route to="/" element={<Home/>}/>
         <Route to="/test" element={<Test/>} />
-      </Routes>
+      </Routes> */}
 
       <Footer/>
     </div>
